e109afe53c182bef58d6e214c96c48caf45acc3a
Philippe Antoniotti <pantoniotti@gmail.com>
2025-12-13 15:34:46 -0500

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const repoName = 'kords'; // just the repo name, not the full URL
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
   plugins: [react(), tailwindcss()],
 });
